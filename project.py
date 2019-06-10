994cd4190d256cb9615b8af40fe7ed4adf434165
William Lathrop <willguy21@gmail.com>
2019-06-10 15:11:28 -0700

    Add files via upload

--- project.py
+++ project.py
@@ -43,35 +43,68 @@ def populateBoard():#Creates peices on board DONE
 
 def recursivePlayerPrimer(x,y): # {[[x,y],[jumped peices],true],
 					#  [x,y] [12, 14] [false]]}
-
-
-	if(BoardList[x][y]) 
-
-def recursivePlayerEngine(x,y, removedList, listToAdd):
+	listOfMoves = []
 
 	tempRemoved = removedList
 
 	if((x-1 > -1) & (y+1 < 8):
 		if(boardList[x-1][y+1] == "  "):
-			#nonrecursive return
-		else if((boardList[x-1][y+1]%2 != boardList[x][y]%2) & (x-2 > -1) & (y+2 < 8)):
-			if(boardList[x-2][y+2] == "  "):
-				boardList[x-2][y+2] == boardList[x][y]
-				tempRemoved.append(boardList[x+1][y+1])
-				#recursive plus list
-				boardList[x-2][y+2] == "  "
+			listToAppend = []
+			listXY = []
+			listJumped = []
+			listXY.append(x-1)
+			listXY.append(y+1)
+			listToAppend.append(listXY)
+			listToAppend.append(listJumped)#done LEFT UPPER
+			listOfMoves.append(listToAppend)
+
+	else if((x-2 > -1) & (y+2 < 8) & (boardList[x-1][y+1]%2 != boardList[x][y]%2)):
+		if(boardList[x-2][y+2] == "  "):
+			boardList[x-2][y+2] == boardList[x][y]
+			tempRemoved.append(boardList[x+1][y+1])
+			listToAppend = []
+			listXY = []
+			listXY.append(x-2)
+			listXY.appennd(y+2)
+			listJumped = []
+			listJumped.append(boardList[x-1][y+1])
+			listToAppend######APPEND CURRENT SOLUTION THEN ADD MORE recursively
+			recursivePlayerEngine(x-2,y+2, tempRemoved, listToAppend)
+
+			############recursive plus list########## LEFT JUMP
+			boardList[x-2][y+2] == "  "
 
 	tempRemoved = removedList
 
 	if((x+1 < 8) & (y+1 < 8):
 		if(boardList[x+1][y+1] == "  "):
-			#no recursive end
+			listToAppend = []
+			listXY = []
+			listJumped = []
+			listXY.append(x+1)
+			listXY.append(y+1)
+			listToAppend.append(listXY)
+			listToAppend.append(listJumped)#done RIGHT UPPER
+			listOfMoves.append(listToAppend)
+
 		else if((boardList[x+1][y+1]%2 != boardList[x][y]%2) & (x+2 < 8) & (y+2 < 8)):
 			if(boardList[x+2][y+2] == "  "):
 				boardList[x+2][y+2] == boardList[x][y]
 				tempRemoved.append(boardList[x+1][y+1])
-				#recursive plus list
+
+
+
+				####################recursive plus list########## RIGHT JUMP - copy above change numbers in coordinates
 				boardList[x+2][y+2] == "  "
+
+
+		##########################Add king functionality
+
+def recursivePlayerEngine(x,y, removedList, listToAdd):
+
+	tempRemoved = removedList
+	##copy second part of above
+
 
 
 	if(BoardList[x][y] > 50):
@@ -105,31 +138,6 @@ def recursivePlayerEngine(x,y, removedList, listToAdd):
 
 
 
-
-
-
-
-
-def isValidMove(x,y):#in progress - FULL REDESIGN - make all valid moves a letter on the board then choose from all letters
-	
-	if(boardList[])
-
-
-	if(BoardList[proX][proY] == "  "):
-		return [True, proX, proY]
-	if((BoardList[proX][proY]%2) != modulus):
-		if(BoardList[proX+(proX-X)])
-
-	
-
-	#check which peice it is
-		#check if its over 50
-			#check all directions
-		#else regular moves
-
-
-
-
 def validateBoard(mod):#Kings pieces and returns false if game is over DONE
 	for x in range(8):
 		if((BoardList[0][x]%2 == 0) & (BoardList[0][x] < 50)):
@@ -149,7 +157,13 @@ def moveForTurn():
 
 
 def AIturn():
+	listOfAI = []
 
+	for x in range (8):
+		for y in range (8):
+			if BoardList[x][y]%2 == 1:
+				tempList = recursiveAIPrimer(x,y)
+				listOfAI.
 
 
 def runGame():
